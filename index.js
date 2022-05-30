2fd6d490d79f8869f372c4df668782392a849d82
Pulkit <297pulkitmehta@gmail.com>
2022-05-30 03:44:25 -0400

    Finished Starter template

---
+++ index.js
@@ -0,0 +1,9 @@
+let button = document.querySelector('.button');
+
+button.addEventListener('mouseup', function(){
+  this.classList.add('active');
+  setTimeout(function () {
+    button.classList.remove('active');
+}, 300);
+});
+
